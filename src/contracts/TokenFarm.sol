a211b3511ae7f9086420d28c41a4c75fb938ed7f
Aarunish Sinha <58175652+aarunishsinha@users.noreply.github.com>
2021-06-25 13:02:34 +0530

    staking tokens by investor and testing

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -9,8 +9,34 @@ contract TokenFarm{
   DappToken public dappToken;
   DaiToken public daiToken;
 
+  address[] public stakers;
+  mapping(address => uint) public stakingBalance;
+  mapping(address => bool) public hasStaked;
+  mapping(address => bool) public isStaking;
+
   constructor(DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
   }
+
+  // Stake Tokens
+  function stakeTokens(uint _amount) public {
+    // transfer Mock DAI tokens to this contract for Staking
+    daiToken.transferFrom(msg.sender, address(this), _amount);
+
+    // Update staking balance
+    stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+    // Add users to stakers array *only* if they haven't staked already
+    if (!hasStaked[msg.sender]) {
+      stakers.push(msg.sender);
+    }
+    // Update staking status
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+  }
+
+  // Unstaking tokens
+
+  // Issuing Tokens
 }
